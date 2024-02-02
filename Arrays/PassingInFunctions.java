4f1faa20e94bd3c916d316639e0730d7fe55315a
Amit <102967201+amitripttyline@users.noreply.github.com>
2024-02-02 20:23:24 +0530

    Create PassingInFunctions.java

---
+++ Arrays/PassingInFunctions.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class PassingInFunctions {
+    public static void main(String[] args) {
+        int[] nums = {8,9,0,8};
+        System.out.println(Arrays.toString(nums));
+        change(nums);
+        System.out.println(Arrays.toString(nums));
+
+    }
+
+    static void change(int[] arr){
+        arr[0]=33;
+    }
+
+
+}
