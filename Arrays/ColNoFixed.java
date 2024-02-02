ce350116f3c64e4e25cab02614a5e6ddd934a261
Amit <102967201+amitripttyline@users.noreply.github.com>
2024-02-02 20:22:24 +0530

    Create ColNoFixed.java

---
+++ Arrays/ColNoFixed.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class ColNoFixed {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1,2,3,4},
+                {5,6},
+                {7,8,9}
+        };
+
+//        for(int i = 0; i < arr.length; i++){
+
+//            System.out.println(Arrays.toString(arr[i]));
+//
+//        }
+
+        for(int row = 0; row < arr.length; row++){
+            for(int col =0; col < arr[row].length; col++){
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+
+        }
+
+
+    }
+}
