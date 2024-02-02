f91fdb8a8bce210e7c422b156743725721077879
Amit <102967201+amitripttyline@users.noreply.github.com>
2024-02-02 20:22:52 +0530

    Create ArrayListExample.java

---
+++ Arrays/ArrayListExample.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>(10);
+//        list.add(44);
+//        list.add(33);
+//        list.add(34);
+//        System.out.println(list);
+//
+//        System.out.println(list.contains(33));
+//        list.set(0,33);
+//        list.remove(2);
+//        System.out.println(list);
+
+        //input
+        for(int i=0; i < 3; i++){
+            list.add(in.nextInt());
+
+        }
+
+        //get any item at any index here
+        for(int i=0; i < 3; i++){
+            System.out.println(list.get(i)); //pass the index here, list[index] will not work here
+
+        }
+
+
+        System.out.println(list);
+    }
+}
