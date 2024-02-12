8c012eab18affc19c93965eda33c8acc0f3cf3a6
Amit <102967201+amitripttyline@users.noreply.github.com>
2024-02-12 23:40:56 +0530

    Linear search code added

---
+++ Searching/LinearSearch.java
@@ -0,0 +1,21 @@
+// Linear Search
+public class Main {
+    public static void main(String[] args) {
+        int target = 14;
+        int[] nums = {22,44,33,22,14,33,44};
+        int ans = linearsearch(nums, target);
+        System.out.println(ans);
+    }
+    public static int linearsearch(int [] arr, int target) {
+        if(arr.length == 0){
+            return -1;
+        }
+        for(int index = 0; index < arr.length; index++){
+            int element = arr[index];
+            if (arr[index] == target){
+                return index;
+            }
+        }
+        return -1;
+    }
+}
