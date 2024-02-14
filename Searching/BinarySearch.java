c30dce32907685162ff3d057fe86cf4092a70f29
Amit <102967201+amitripttyline@users.noreply.github.com>
2024-02-14 06:20:17 +0530

    Create BinarySearch

---
+++ Searching/BinarySearch.java
@@ -0,0 +1,24 @@
+// binary search(assumed array is sorted ina ascending/descending order)
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {3,5,7,8,9,33,55};
+        int target = 33;
+        int index = binarysearch(arr, target);
+        System.out.println(index);
+    }
+    static int binarysearch(int[] arr, int target){
+        int start = 0;
+        int end = arr.length -1;
+        while(start <= end){
+            int mid = start + (end - start)/2; // finding the middle element, also might possible that (start + end ) exceeds the range so we took in fraction and all
+            if(arr[mid] > target){
+                end = mid - 1;
+            } else if(arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
