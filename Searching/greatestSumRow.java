28a2ae844084723d637a53c51b105566c3cdbf23
Amit <102967201+amitripttyline@users.noreply.github.com>
2024-02-14 05:41:38 +0530

    Find greatest Sum of Row.

---
+++ Searching/greatestSumRow.java
@@ -0,0 +1,26 @@
+public class greatestSumRow {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {22,44,55},
+                {44,55,55,11},
+                {1,4,5}
+        };
+        int max = maxrow(arr);
+        System.out.println(max);
+    }
+
+    static int maxrow(int[][] arr){
+        int rowsum = 0;
+        for(int row = 0; row < arr.length; row++){
+            int sumrow = 0;
+            for (int col=0; col < arr[row].length; col++){
+                sumrow += arr[row][col];
+            }
+            if(sumrow > rowsum){
+                rowsum = sumrow;
+            }
+            return rowsum;
+        }
+        return -1;
+    }
+}
