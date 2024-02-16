80de39604314745840a93f0c05c431cb58080836
Amit <102967201+amitripttyline@users.noreply.github.com>
2024-02-16 18:36:51 +0530

    Search element in RowColMatrix

---
+++ Searching/RowColMatrix.java
@@ -0,0 +1,33 @@
+//Search element in 2D array
+import java.util.Arrays;
+
+public class RowColMatrix {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {2,7,8,12},
+                {9,8,22,33},
+                {10,16,28,44}
+        };
+        int target = 9;
+        int[] arr2 = searchIn2Darray(arr, target);
+        System.out.println(Arrays.toString(arr2));
+
+    }
+
+    static int[] searchIn2Darray(int arr[][], int target) {
+        int r = 0;
+        int c = arr.length;
+        while(r < arr.length & c > 0){
+            if(arr[r][c] == target){
+                return new int[]{r, c};
+            }
+
+            if(arr[r][c] < target){
+                r++;
+            } else {
+                c--;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
