7fb03d93607ff6f51233ebbf1a510c5ffa1dcb54
Amit <102967201+amitripttyline@users.noreply.github.com>
2024-02-19 23:43:29 +0530

    Create cyclicSort.java

---
+++ sorting/cyclicSort.java
@@ -0,0 +1,9 @@
+public class cyclicSort {
+    public static void main(String[] args) {
+        int[] arr = {2,4,3,1,5};
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    
+}
