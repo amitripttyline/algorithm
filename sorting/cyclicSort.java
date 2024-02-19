7622da351f6f72cbc973c2ae65b3185013e007f1
Amit <102967201+amitripttyline@users.noreply.github.com>
2024-02-20 00:25:23 +0530

    Update cyclicSort.java

--- sorting/cyclicSort.java
+++ sorting/cyclicSort.java
@@ -1,9 +1,27 @@
+import java.util.Arrays;
+
 public class cyclicSort {
     public static void main(String[] args) {
         int[] arr = {2,4,3,1,5};
-        selection(arr);
+        cyclic(arr);
         System.out.println(Arrays.toString(arr));
     }
 
+    static void cyclic(int[] arr) {
+        int i =0;
+        while(i < arr.length){
+            int correct = arr[i] - 1;
+            if(arr[i] != arr[correct]){
+                swap(arr, i, correct );
+            } else {
+                i++;
+            }
+        }
+    }
     
+    static void swap(int[] arr, int maxIndex, int end){
+        int temp = arr[maxIndex];
+        arr[maxIndex] = arr[end];
+        arr[end] = temp;
+    }
 }
